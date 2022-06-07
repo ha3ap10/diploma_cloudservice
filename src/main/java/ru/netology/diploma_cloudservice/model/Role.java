cb6880e2c536ff32045f395d6f95dd45a3913748
anazarenko <anazarenko@data-platform.ru>
2022-06-07 14:21:46 +0300

    missed Role added

---
+++ src/main/java/ru/netology/diploma_cloudservice/model/Role.java
@@ -0,0 +1,18 @@
+package ru.netology.diploma_cloudservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role implements GrantedAuthority {
+
+    public static final String USER_ADMIN = "ROLE_USER_ADMIN";
+    public static final String AUTHOR_ADMIN = "ROLE_AUTHOR_ADMIN";
+    public static final String BOOK_ADMIN = "ROLE_BOOK_ADMIN";
+
+    private String authority;
+}
